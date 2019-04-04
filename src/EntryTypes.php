3816257159ce44ecf5c0661567ba56df21438df1
James Edmonston <james@ethercreative.co.uk>
2019-04-04 14:30:02 +0100

    Add entry types as children

--- src/EntryTypes.php
+++ src/EntryTypes.php
@@ -1,80 +1,95 @@
 <?php
 /**
- * Entry Types plugin for Craft CMS 3.x
- *
- * Easily switch between entry types in entries section
- *
- * @link      https://ethercreative.co.uk
- * @copyright Copyright (c) 2019 Ether Creative
- */
+* Entry Types plugin for Craft CMS 3.x
+*
+* Easily switch between entry types in entries section
+*
+* @link      https://ethercreative.co.uk
+* @copyright Copyright (c) 2019 Ether Creative
+*/
 
 namespace ether\entrytypes;
 
-
 use Craft;
 use craft\base\Plugin;
-use craft\services\Plugins;
-use craft\events\PluginEvent;
+use craft\base\Element;
+use craft\elements\Entry;
+use craft\models\Section;
+use craft\events\RegisterElementSourcesEvent;
 
 use yii\base\Event;
 
 /**
- * Class EntryTypes
- *
- * @author    Ether Creative
- * @package   EntryTypes
- * @since     1.0.0
- *
- */
+* Class EntryTypes
+*
+* @author    Ether Creative
+* @package   EntryTypes
+* @since     1.0.0
+*
+*/
 class EntryTypes extends Plugin
 {
-    // Static Properties
-    // =========================================================================
+  // Static Properties
+  // =========================================================================
 
-    /**
-     * @var EntryTypes
-     */
-    public static $plugin;
+  /**
+  * @var EntryTypes
+  */
+  public static $plugin;
 
-    // Public Properties
-    // =========================================================================
+  // Public Properties
+  // =========================================================================
 
-    /**
-     * @var string
-     */
-    public $schemaVersion = '1.0.0';
+  /**
+  * @var string
+  */
+  public $schemaVersion = '1.0.0';
 
-    // Public Methods
-    // =========================================================================
+  // Public Methods
+  // =========================================================================
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-        self::$plugin = $this;
+  /**
+  * @inheritdoc
+  */
+  public function init()
+  {
+    parent::init();
+    self::$plugin = $this;
 
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                }
-            }
-        );
+    Event::on(Entry::class, Element::EVENT_REGISTER_SOURCES, function(RegisterElementSourcesEvent $event) {
+      $children = [];
 
-        Craft::info(
-            Craft::t(
-                'entry-types',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
+      foreach ($event->sources as $i => $source) {
+        if (!isset($source['data'])) {
+          continue;
+        }
+
+        $children[$i] = [];
+        $entryTypes = Craft::$app->sections->getEntryTypesBySectionId($source['criteria']['sectionId']);
+
+        foreach ($entryTypes as $entryType) {
+          $children[$i][] = [
+            'key' => 'section:' . $entryType->uid,
+            'label' => $entryType->name,
+            'data' => [
+              'has-structure' => true,
+              'default-sort' => 'structure:asc',
+              'type' => 'structure',
+              'handle' => $entryType->handle,
+              'entry-type' => true
+            ],
+            'criteria' => [
+              'sectionId' => $entryType->sectionId,
+              'type' => $entryType->handle,
+              'editable' => false,
+              ]
+            ];
+          }
+        }
+
+        foreach ($children as $key => $child) {
+          $event->sources[$key]['nested'] = $child;
+        }
+      });
     }
-
-    // Protected Methods
-    // =========================================================================
-
-}
+  }
